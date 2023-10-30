43abdfee6b0eeb72ce63542876210593839b534f
Jaymoo87 <114165259+Jaymoo87@users.noreply.github.com>
2023-10-30 17:32:12 -0500

    document list structure layed out, expanded list formatted correctly for nested notes

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,9 +1,17 @@
 "use client";
 
-import { Id } from "@/convex/_generated/dataModel";
-import { cn } from "@/lib/utils";
-import { ChevronDown, ChevronRight, LucideIcon } from "lucide-react";
 import React from "react";
+import { useRouter } from "next/navigation";
+
+import { Id } from "@/convex/_generated/dataModel";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+
+import { cn } from "@/lib/utils";
+import { toast } from "sonner";
+
+import { Skeleton } from "@/components/ui/skeleton";
+import { ChevronDown, ChevronRight, LucideIcon, Plus } from "lucide-react";
 
 interface ItemProps {
   id?: Id<"documents">;
@@ -30,20 +38,48 @@ export const Item = ({
   onExpand,
   expanded,
 }: ItemProps) => {
+  const router = useRouter();
+  const create = useMutation(api.documents.create);
+
+  const handleExpand = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.stopPropagation();
+    onExpand?.();
+  };
+
+  const onCreate = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.stopPropagation();
+    if (!id) return;
+    const promise = create({ title: "untitled", parentDocument: id }).then((documentId) => {
+      if (!expanded) {
+        onExpand?.();
+      }
+      // router.push(`/documents/${documentId}`);
+    });
+    toast.promise(promise, {
+      loading: "Creating new Memory",
+      success: "New Memory Created",
+      error: "Did Not Create a Memory, Remember Better",
+    });
+  };
+
   const ChevronIcon = expanded ? ChevronDown : ChevronRight;
 
   return (
     <div
       onClick={onClick}
       role="button"
-      style={{ paddingLeft: level ? `${level * 12 + 12}` : "12px" }}
+      style={{ paddingLeft: level ? `${level * 12 + 12}px` : "12px" }}
       className={cn(
         "group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium",
         active && "bg-primary/5 text-primary"
       )}
     >
       {!!id && (
-        <div role="button" className="h-full rounded-sm hover:bg-neutral-300 dark: bg-neutral-600 mr-1">
+        <div
+          role="button"
+          className="h-full rounded-sm hover:bg-neutral-300 dark: bg-neutral-600 mr-1"
+          onClick={handleExpand}
+        >
           <ChevronIcon className="h-4 w-4 shrink-0 text-muted-foreground/50" />
         </div>
       )}
@@ -60,6 +96,26 @@ export const Item = ({
           <span className="text-xs">CMD</span>K
         </kbd>
       )}
+      {!!id && (
+        <div className="ml-auto flex items-center gap-x-2">
+          <div
+            role="button"
+            onClick={onCreate}
+            className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600"
+          >
+            <Plus className="h-4 w-4 text-muted-foreground" />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+Item.Skeleton = function ItemSkeleton({ level }: { level?: number }) {
+  return (
+    <div style={{ paddingLeft: level ? `${level * 12 + 25}` : "12px" }} className="flex gap-x-2 py-[3px]">
+      <Skeleton className="h-4 w-4" />
+      <Skeleton className="h-4 w-[30%]" />
     </div>
   );
 };
