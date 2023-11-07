373711ca0be1e435da9f62a6083ac94f11e6feab
Jaymoo87 <114165259+Jaymoo87@users.noreply.github.com>
2023-11-07 09:09:08 -0600

    added UI functionality for delete and restore, shadcn input and alerts doin the heavy work, input for searching notes

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -13,10 +13,11 @@ import { toast } from "sonner";
 import { ChevronsLeft, MenuIcon, Plus, PlusCircle, Search, Settings, Trash } from "lucide-react";
 
 import { Popover, PopoverTrigger, PopoverContent } from "@/components/ui/popover";
-import UserItem from "./user-items";
-import { Item } from "./item";
 
+import { Item } from "./item";
+import UserItem from "./user-items";
 import DocumentList from "./document-list";
+import TrashBox from "./trash-box";
 
 export const Navigation = () => {
   const pathname = usePathname();
@@ -150,7 +151,7 @@ export const Navigation = () => {
               <Item label="Trash" icon={Trash} />
             </PopoverTrigger>
             <PopoverContent className="p-0 w-72" side={isMobile ? "bottom" : "right"}>
-              <p>Forgotten Memories</p>
+              <TrashBox />
             </PopoverContent>
           </Popover>
         </div>
